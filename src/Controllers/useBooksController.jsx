832216f5a52d7005f7b514815d3beef608fc400f
Yawarmurtaza <yawar_yawar@hotmail.com>
2022-10-25 21:50:26 -0700

    new authors and new publisher are not being displayed when adding a newbook. App is not in error

--- src/Controllers/useBooksController.jsx
+++ src/Controllers/useBooksController.jsx
@@ -8,16 +8,31 @@ export default function useBooksController() {
     const { pubsData, pubsDataError, AddPubEntity } = usePublishersEntityManager();
 
     function AddNewBook(title, authorNames, publisherName, publishDate) {
-        const publisherId = pubsData ? pubsData.find(p => p.name == publisherName.trim()).id : 0;
+        const publisherId = pubsData ? pubsData.find(p => p.name == publisherName.trim())?.id : 0;
         if (publisherId === 0) {
             AddPubEntity(publisherName);
         }
         const authorNamesArray = authorNames ?
             authorNames.split(",").filter(name => name && name.length > 0)
-                .map(name => name.trim()) : 
-                [];
-                
+                .map(name => name.trim()) :
+            [];
+
+        const existingAuthorNames = authorsData.map(a => a.name);
+
+        const newAuthNames =  authorNamesArray.filter(name => !existingAuthorNames.includes(name)) + "";
+        console.log("New Auth Names = " + newAuthNames);
+        const newAuthNamesArray = newAuthNames.split(",");
+        let newAuthIds = [];
+        newAuthNamesArray.forEach(name => {
+            newAuthIds.push(AddAuthorEntity(name));
+        });
+
+
+
+        
         const authorIds = authorsData?.filter(a => authorNamesArray.includes(a.name)).map(a => a.id);
+        authorIds.push(newAuthIds);
+        console.log("authorIds = " + authorIds);
         return AddBookEntity(title, authorIds, publisherId, publishDate);
     }
     console.log("Books Controller");
