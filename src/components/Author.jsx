2ba92f50ff548b7423402a056f7a4b2d973c73bd
yawarmurtaza <yawar_yawar@hotmail.com>
2022-10-26 17:08:59 -0700

    Authors tab is showing their books. App is working

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,7 +1,39 @@
-export default function Author(props) {    
+import { useContext } from "react";
+import { AuthorContext, BookContext } from "./App";
+
+export default function Author(props) {
+
+    const { booksData } = useContext(BookContext);
+    const { authorsData } = useContext(AuthorContext);
+
+    function PrintBooksForAuthor() {
+        const currentAuthId = props.author.id;
+        let booksByAuthor = [];
+        booksData.forEach(book => {
+
+            if (book.authorIds.includes(currentAuthId)) {
+
+                booksByAuthor.push(book);
+            }
+
+        });
+
+        let bookNames = [];
+        booksByAuthor.forEach(book => {
+            bookNames.push(<li key={book.id}> {book.title} </li>);
+        });
+
+        return (bookNames);
+    }
+
     return (<div className="col-md-4 single-note-item all-category">
         <div className="card card-body">
             <h5 className="note-title text-truncatew-75 mb-0">{props.author.name}</h5>
+            <div className="note-content">
+                <ul>
+                    <PrintBooksForAuthor />
+                </ul>
+            </div>
 
         </div>
     </div>
