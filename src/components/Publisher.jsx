d142363d35db98e200d3d315e92885ec1af33319
yawarmurtaza <yawar_yawar@hotmail.com>
2022-10-26 18:27:17 -0700

    publishers page shows books. App is working

--- src/components/Publisher.jsx
+++ src/components/Publisher.jsx
@@ -1,8 +1,35 @@
+import { useContext } from "react";
+import { BookContext } from "./App";
+
 export default function Publisher(props) {    
+    const { booksData } = useContext(BookContext);
+    function PrintPBooksOfPublisher(){
+        const currentPubId = props.publisher.id;
+        let publishersBooks = [];
+        booksData.forEach(book => {
+            if(book.publisherId === currentPubId){
+                publishersBooks.push(book);
+            }
+        });
+
+        let booksToPrint = [];
+        publishersBooks.forEach(book => {
+            
+            booksToPrint.push(<li key={book.id}>{book.title}</li>);
+        });
+
+        return (booksToPrint);
+
+    }
+
     return (<div className="col-md-4 single-note-item all-category">
         <div className="card card-body">
             <h5 className="note-title text-truncatew-75 mb-0">{props.publisher.name}</h5>
-
+            <div className="note-content">
+                <ul>
+                    <PrintPBooksOfPublisher />
+                </ul>
+            </div>
         </div>
     </div>
     );
